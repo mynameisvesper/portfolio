35e5fb686997f1823d793b1c527940c90e939379
mynameisvesper <125198482+mynameisvesper@users.noreply.github.com>
2023-02-18 12:42:34 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, render_template, request
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'gkswngh14@gmail.com'
+app.config['MAIL_PASSWORD'] = 'tgawdqsinbnjyrdp'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['DEBUG'] = True
+app.config['TESTING'] = False
+
+app.template_folder = os.path.abspath('templates')
+
+mail = Mail(app)
+
+# Define a new function that sends a test email
+def send_test_email():
+    with app.app_context():
+        msg = Message('Test Message', sender='youremail@gmail.com', recipients=['youremail@gmail.com'])
+        msg.body = 'This is a test message'
+        mail.send(msg)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    msg = Message('New message from your website', sender='gkswngh14@gmail.com', recipients=['gkswngh14@gmail.com'])
+    msg.body = f'Name: {name}\nEmail: {email}\nMessage: {message}'
+
+    try:
+        mail.send(msg)
+        return 'success'
+    except Exception as e:
+         print(e)
+         return str(e)
+    
+    if __name__ == '__main__':
+        send_test_email()
+        app.run(debug=True)
